8035171d2d89ac1d60b7a0eb2cadd9b9b3e19ed8
Joe Scott <jbs198585@gmail.com>
2022-02-05 13:03:52 -0500

    Else-if

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-let random = Math.random();
-if (random < 0.5) {
-  console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-  console.log(random);
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//   console.log(random);
+// }
 
-if (random >= 0.5) {
-  console.log("YOUR NUMBER IS GREATER THAN O.5!!!");
-  console.log(random);
+// if (random >= 0.5) {
+//   console.log("YOUR NUMBER IS GREATER THAN O.5!!!");
+//   console.log(random);
+// }
+
+// const dayOfWeek = "Monday";
+
+// if (dayOfWeek === "Monday") {
+//   console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === "Saturday") {
+//   console.log("YAY I LOVE SATURDAYS");
+// }
+
+// 0-5 - FREE
+// 5 - 10 CHILD $10
+// 10 - 65 ADULT $20
+// 65+ SENIOR $10
+
+const age = 8;
+
+if (age < 5) {
+  console.log("You are a baby. You get in for free!");
+} else if (age < 10) {
+  console.log("You are a child. You pay $10.");
+} else if (age < 65) {
+  console.log("You are an adult. you pay $20.");
 }
