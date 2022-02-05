3d17047d7ff33f617e15c6e2d3a3a37763cb72a3
Joe Scott <jbs198585@gmail.com>
2022-02-05 13:22:25 -0500

    nesting conditonals

--- app.js
+++ app.js
@@ -22,12 +22,26 @@
 // 10 - 65 ADULT $20
 // 65+ SENIOR $10
 
-const age = 8;
+// const age = 8;
 
-if (age < 5) {
-  console.log("You are a baby. You get in for free!");
-} else if (age < 10) {
-  console.log("You are a child. You pay $10.");
-} else if (age < 65) {
-  console.log("You are an adult. you pay $20.");
+// if (age < 5) {
+//   console.log("You are a baby. You get in for free!");
+// } else if (age < 10) {
+//   console.log("You are a child. You pay $10.");
+// } else if (age < 65) {
+//   console.log("You are an adult. you pay $20.");
+// }
+
+const password = prompt("please enter a new password");
+
+// Password must be 6+ characters
+if (password.length >= 6) {
+  // Password cannot include space
+  if (password.indexOf(" ") === -1) {
+    console.log("Valid Password!");
+  } else {
+    console.log("Password cannot contain spaces!");
+  }
+} else {
+  console.log("PASSWORD TOO SHORT! Must be 6+ characters.");
 }
